a072be860b2610705798e68fdafd065e0e1cb044
Khamidullaev_Khamidulla <Khamidullaev_007@mail.ru>
2021-11-18 19:22:43 +0500

    Lesson 11

---
+++ Lesson.py
@@ -0,0 +1,31 @@
+# with open('abc.txt') as abc:
+#     with open ('abcd', 'w') as abcd:
+#             for line in abc:
+#                 abcd.write(line)
+
+
+
+# with open('AMG.jpg', 'rb') as img:
+#     with open('AMG_copy.jpg', 'wb') as copy:
+#         for line in img:
+#             copy.write(line)
+
+# import os 
+
+# os.rename('hello.py', 'bye.txt')
+# print(os.path.split('abc.txt'))
+
+
+# name = input('Ism')
+# surname = input('Fam')
+
+# with open('./abc.txt', 'r+') as students:
+#     print(abc.txt.())
+
+login = input('Enter Login: ')
+parol = input('Enter Password: ')
+
+from datetime import datetime
+
+date = datetime.now()
+print('Welcome', login, date)
